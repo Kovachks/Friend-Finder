f91cd95435f5cd7171d703bdb7fb81846f89f00b
Keith Kovach <kovachks90@gmail.com>
2017-09-03 13:28:21 -0400

    mapping of routes working

--- server.js
+++ server.js
@@ -6,58 +6,27 @@ var path = require("path");
 var app = express();
 var PORT = process.env.PORT || 3000;
 
-var totalDifference = 0;
-
-var newUser = [];
-
-var totalDifferenceArray = [];
-
 app.use(bodyParser.json());
 app.use(bodyParser.urlencoded({ extended: true }));
 app.use(bodyParser.text());
 app.use(bodyParser.json({ type: "application/vnd.api+json" }));
 
-var routes = require("./app/routing/htmlRoutes.js");
+require("./app/routing/htmlRoutes.js")(app);
 
-app.use(express.static(__dirname + "/app/public"))
+require("./app/routing/apiRoutes.js")(app);
 
-app.use("/", routes);
+// app.use(express.static(__dirname + "/app/public"));
+
+// app.use("/api/friends", apiRoutes);
+
+// app.use("/", routes);
+
+// app.use("/survey", routes);
+
+// app.use("/api/new", apiRoutes);
 
 
-app.get("/api/friends", function(req, res) {
-	res.json(newUser);
-})
-app.post("/api/new", function(req, res) {
-	console.log("RESPONSE: " + res)
-	totalDifferenceArray = [];
-	var newUser2 = req.body;
-	console.log(newUser2);
-	newUser.push(newUser2);
-	res.json(newUser2);
-	console.log(newUser)
-	for (var i = 0; i < newUser.length - 1; i += 1) {
-		for (var g = 0; g < 10; g += 1) {
-			totalDifference += Math.abs(newUser2.score[g] - newUser[i].score[g])
-		}
-	console.log("Total Difference: " + totalDifference);
-	totalDifferenceArray.push(totalDifference);
-	console.log("Total Difference Array: " + totalDifferenceArray);
-	totalDifference = 0;
-	}
-	var closeUserValue = Math.min.apply(null, totalDifferenceArray);
-	console.log("close User Value: " + closeUserValue)
-	var closeUser = totalDifferenceArray.indexOf(closeUserValue)
-	console.log("Closest user: " + closeUser);
-	var closeUserName = newUser[closeUser].name;
-	var closeUserImage = newUser[closeUser].photo;
-	var closeUserData = [];
-	closeUserData.push(closeUserName);
-	closeUserData.push(closeUserImage);
-	console.log("Closest User Name/Image: " + closeUserData)
-	console.log("Name of Closest User " + closeUserData[0])
-	console.log("image of Closest User " + closeUserData[1])
-	module.exports = closeUserData;
-})
+
 app.listen(PORT, function() {
 	console.log("App listening on PORT " + PORT)
-})	
+})
