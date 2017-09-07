cab6be29fcd91d65547b297c5ef45f7c4bd76d9d
Keith Kovach <kovachks90@gmail.com>
2017-09-06 23:24:49 -0400

    updated styling

--- app/routing/htmlRoutes.js
+++ app/routing/htmlRoutes.js
@@ -1,11 +1,16 @@
+//Requiring relavent NPM's
 var path = require("path");
 
+//Exporting functions to server.js
 module.exports = function(app) {
+
+	//Loading the home.html page when the "/" path is hit
 	app.get("/", function(req, res) {
 		res.sendFile(path.join(__dirname, "../public/home.html"))
 	})
-	app.get("/survey.html?", function(req, res) {
+
+	//Loading the survey.html page when the /survey path is hit
+	app.get("/survey", function(req, res) {
 		res.sendFile(path.join(__dirname, "../public/survey.html"))
 	})
-
 }
